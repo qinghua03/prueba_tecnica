c936d99ff47093534c3bba7352a4516bb4f154fc
Qinghua Liang <lqh940316@gmail.com>
2022-10-10 00:58:44 +0200

    subida

---
+++ prueba.py
@@ -0,0 +1,61 @@
+from flask import Flask
+import os
+from flask import request
+import pickle
+import numpy as np
+import re
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+from collections import Counter
+import pandas as pd
+
+os.chdir(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+signos = re.compile("(\...)|(\_)|(\\n)|(\#)|(\.)|(\;)|(\:)|(\!)|(\?)|(\¿)|(\@)|(\,)|(\")|(\()|(\))|(\[)|(\])|(\d+)")
+
+def signs_tweets(tweet):
+    return signos.sub('', tweet.lower())
+
+def remove_links(df):
+    return " ".join(['{link}' if ('http') in word else word for word in df.split()])
+
+spanish_stopwords = stopwords.words('spanish')
+
+def remove_stopwords(df):
+    return " ".join([word for word in df.split() if word not in spanish_stopwords])
+
+
+def spanish_stemmer(x):
+    stemmer = SnowballStemmer('spanish')
+    return " ".join([stemmer.stem(word) for word in x.split()])
+
+
+@app.route("/", methods=['GET'])
+def hello():
+    return "Bienvenido a mi API del modelo"
+
+# 1. Devolver la predicción de los nuevos datos enviados mediante argumentos en la llamada
+@app.route('/predict', methods=['GET'])
+def predict():
+   
+    model = pickle.load(open('model/sentiment_model','rb'))
+
+    text = str(request.args["text"])
+
+    #text=nito.clean_emoji(text)
+    text=signs_tweets(text)
+    text=remove_links(text)
+    text=remove_stopwords(text)
+    text=spanish_stemmer(text)
+
+
+    X = []
+    X.append(text)
+
+    result = model.predict(X)
+
+    return "la prediccion es" + " " +str(result)
+#app.run()
